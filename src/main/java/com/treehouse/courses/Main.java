88ee5daedaa8cfa708eadcb3d1d9706e646a0d14
Jordan Mor <jordanmor@gmail.com>
2019-07-20 19:59:53 -0400

    Add sign in form and template

--- src/main/java/com/treehouse/courses/Main.java
+++ src/main/java/com/treehouse/courses/Main.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import static com.treehouse.courses.RenderUtilities.render;
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,12 @@ public class Main {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index");
+        });
+
+        post("/sign-in", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("username", req.queryParams("username"));
+            return render(model, "sign-in");
         });
     }
 }
