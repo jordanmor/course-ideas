91015cf3dc50eae74d9a78696ac08f4e4cb45fcf
Jordan Mor <jordanmor@gmail.com>
2019-07-22 14:00:51 -0400

    Use Spark filters and attributes for DRYer code

--- src/main/java/com/treehouse/courses/Main.java
+++ src/main/java/com/treehouse/courses/Main.java
@@ -16,13 +16,26 @@ public class Main {
 
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
+        before((req, res) -> {
+            if(req.cookie("username") !=  null) {
+                req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        before("/ideas" ,(req, res) -> {
+            if(req.attribute("username") == null) {
+                res.redirect("/");
+                halt();
+            }
+        });
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
+            model.put("username", req.attribute("username"));
             return render(model, "index");
         });
 
-        post("/", (req, res) -> {
+        post("/sign-in", (req, res) -> {
             String username = req.queryParams("username");
             res.cookie("username", username);
             res.redirect("/");
@@ -37,8 +50,7 @@ public class Main {
 
         post("/ideas", (req, res) -> {
             String title = req.queryParams("title");
-            // TODO:csd - This username is tied to the cookie implementation
-            CourseIdea courseIdea = new CourseIdea(title, req.cookie("username"));
+            CourseIdea courseIdea = new CourseIdea(title, req.attribute("username"));
             dao.add(courseIdea);
             res.redirect("/ideas");
             return null;
